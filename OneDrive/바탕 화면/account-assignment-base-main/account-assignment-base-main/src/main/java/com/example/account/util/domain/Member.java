39d5ce6771f33a2ca42822f1e41f059c901664c5
ujii <wcm9763@naver.com>
2024-05-12 23:45:59 +0900

    ✨ 회원가입 API

---
+++ OneDrive/바탕 화면/account-assignment-base-main/account-assignment-base-main/src/main/java/com/example/account/util/domain/Member.java
@@ -0,0 +1,34 @@
+package com.example.account.util.domain;
+
+import com.example.account.util.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "MEMBERS")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+}
---
+++ OneDrive/바탕 화면/account-assignment-base-main/account-assignment-base-main/src/main/java/com/example/account/util/dto/UserLoginDto.java
@@ -0,0 +1,15 @@
+package com.example.account.util.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDto {
+    private String userId;
+    private String password;
+}
